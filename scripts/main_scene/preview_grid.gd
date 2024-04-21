a64a850c1c2d91d7b674472e4855e49506f59e7d
Lauren Oopsum <lklalakla@gmail.com>
2024-04-21 13:20:20 -1000

    Phrases are automatically added to the preview

--- scripts/main_scene/preview_grid.gd
+++ scripts/main_scene/preview_grid.gd
@@ -4,7 +4,7 @@ var gloss_grid : GridContainer
 var phrase : GlossNode
 
 func create_preview(rows_data : Array) :
-	phrase = set_phrase()
+	set_phrase()
 	if V.end : print("Multiple phrases not yet supported")
 	else : 
 		for template in rows_data :
@@ -15,7 +15,6 @@ func create_preview(rows_data : Array) :
 ## Creates a non-grid row in the preview
 func create_single_row(template : Array) :
 	var type := C.TYPE_NAMES.find(template[0].to_lower())
-	print(type)
 	match type :
 		C.TYPE.PARAGRAPH : print("Pass paragraph")
 		C.TYPE.PHRASE : new_phrase(template)
@@ -23,25 +22,36 @@ func create_single_row(template : Array) :
 
 func new_phrase(template : Array) :
 	var phrase_label := Label.new()
+	var row_template : PoolStringArray = template[1].split("-")
+	print(row_template)
+	for item in phrase.get_children() :
+		if item.node_type == "item" :
+			var is_match := true
+			for att in item.get_attribute_list() :
+				if !row_template.has(item.attributes[att]) : is_match = false
+			if is_match : 
+				phrase_label.text = item.node_value
+				add_child(phrase_label)
+				return
 
 
 func set_phrase() :
-	print("Set phrase")
 	_grab_phrase(V.gloss_tree)
 
 
 func _grab_phrase(node : Node) :
 	for item in node.get_children() :
 		if node is GlossNode && C.TYPE_NAMES.find(node.node_type) == C.TYPE.PHRASE :
-			print(node.node_type)
 			if item.node_type == "item" && is_set_phrase(item) :
 				phrase = node
+				print("Set node to phrase")
 		else : _grab_phrase(item)
 
 
 func is_set_phrase(phrase : GlossNode) -> bool :
 	for x in phrase.attributes :
 		if phrase.attributes[x] == "segnum" && phrase.node_value == str(V.start) :
+			print("SET PHRASE")
 			return true
 	return false
 
